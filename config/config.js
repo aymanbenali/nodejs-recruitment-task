e82bb5ac4e8c4b4013326cf494752c8cf2d8f772
ayman <aymanbenali15@gmail.com>
2022-04-23 14:25:25 +0000

    Feature: Add sequelize as an orm with mysql

---
+++ config/config.js
@@ -0,0 +1,32 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { MYSQL_SERVER, MYSQL_PORT, MYSQL_DATABASE, MYSQL_USER, MYSQL_PASSWORD } =
+  process.env || {};
+
+module.exports = {
+  development: {
+    username: MYSQL_USER,
+    password: MYSQL_PASSWORD,
+    database: MYSQL_DATABASE,
+    host: MYSQL_SERVER,
+    port: MYSQL_PORT,
+    dialect: "mariadb",
+  },
+  test: {
+    username: MYSQL_USER,
+    password: MYSQL_PASSWORD,
+    database: MYSQL_DATABASE,
+    host: MYSQL_SERVER,
+    port: MYSQL_PORT,
+    dialect: "mariadb",
+  },
+  production: {
+    username: MYSQL_USER,
+    password: MYSQL_PASSWORD,
+    database: MYSQL_DATABASE,
+    host: MYSQL_SERVER,
+    port: MYSQL_PORT,
+    dialect: "mariadb",
+  },
+};
